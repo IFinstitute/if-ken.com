15df751f99917baa95e40189887a7ac9769c9e60
IFinstitute <m.yosuke119@gmail.com>
2026-01-01 18:37:13 +0900

    update node v20.x

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',  // 静的サイトとして書き出すための設定
-  basePath: '/if-ken.com', // GitHubのリポジトリ名を / で始めて書く
-  images: {
-    unoptimized: true, // GitHub Pagesで画像を表示させるために必要
-  },
+  /* Vercel では output: 'export' や basePath は不要です */
+  // output: 'export',  // 静的サイトとして書き出すための設定
+  // basePath: '/if-ken.com', // GitHubのリポジトリ名を / で始めて書く
+  // images: {
+  //   unoptimized: true, // GitHub Pagesで画像を表示させるために必要
+  // },
 };
 
 module.exports = nextConfig;
